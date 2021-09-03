6fc63d03e330f4cb3334adb3c20a10faddbcbd1f
zummers <83545395+zummers@users.noreply.github.com>
2021-09-03 18:50:55 +0100

    Add files via upload

---
+++ C36RV_SpeedRacer_StudentActivity-main/js/Form.js
@@ -0,0 +1,33 @@
+class Form {
+  constructor() {
+    this.input = createInput("").attribute("placeholder", "Enter your name");
+    this.playButton = createButton("Play");
+    this.titleImg = createImg("./assets/title.png", "game title");
+    this.greeting = createElement("h2");
+  }
+
+  setElementsPosition() {
+    this.titleImg.position(120,50);
+    this.input.position(width / 2 - 110, height / 2 - 80);
+    this.playButton.position(width / 2 - 90, height / 2 - 20);
+    this.greeting.position(width / 2 - 300, height / 2 - 100);
+  }
+
+  setElementsStyle() {
+    this.titleImg.class("gameTitle");
+    this.input.class("customInput");
+    this.playButton.class("customButton");
+    this.greeting.class("greeting");
+  }
+
+  hide() {
+    this.greeting.hide();
+    this.playButton.hide();
+    this.input.hide();
+  }
+
+  display() {
+    this.setElementsPosition();
+    this.setElementsStyle();
+  }
+}
